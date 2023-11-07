d3fdfba8732ae172c78299c8c34565ba9432b165
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-11-07 18:56:51 +0100

    progress bar with cleanup function

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const ProgressBar = ({timer}) => {
+    const [remainingTime, setRemainingTime] = useState(timer);
+
+    useEffect(() => {
+        console.log("interval")
+        const interval = setInterval(() => {
+            setRemainingTime(prev => prev - 10)
+        }, 10)
+        return () => {
+            clearInterval(interval)
+        }
+    }, []);
+    return (
+        <progress value={remainingTime} max={timer}/>
+    )
+};
+
+export default ProgressBar
