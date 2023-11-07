adda75df7248522302ada3fc199e99a4a9da3b18
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-11-07 18:58:11 +0100

    useEffect for cleanup to avoid infinite loop

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
+
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+  useEffect(() => {
+     const interval = setTimeout(() => {
+      onConfirm()
+    },TIMER);
+    return () => {
+      clearTimeout(interval)
+    }
+  }, [onConfirm]) 
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +25,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
