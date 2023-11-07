b3c24939d38fa265e8409c94a6a7acc87b2ab9a5
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-11-07 18:58:31 +0100

    modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,44 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef, useEffect, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
-  const dialog = useRef();
+const Modal = (props) => {
+  // const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  // if(isopen) {
+  //   dialog.current.showModal()
+  // }
 
-  return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
-    </dialog>,
-    document.getElementById('modal')
-  );
-});
+  // useEffect(() => {
+  //   if (isopen) {
+  //     dialog.current.showModal();
+  //   } else {
+  //     dialog.current.close();
+  //   }
+  // }, [isopen]);
+
+
+  // return createPortal(
+  //   <Fragment>
+  //     <div className='overlay'></div>
+  //     <div className="modal" onClick={onClose}>
+  //       {children}
+  //     </div>
+  //   </Fragment>,
+  //   document.getElementById('modal')
+  // );
+  const Backdrop = () => {
+    return <div className='overlay' onClick={props.onClick}></div>
+  }
+
+  const Modal = () => {
+    return <div className='modal'>{props.children}</div>
+  }
+  return (
+    <Fragment>
+      {createPortal(<Backdrop onClick={props.onClick} />, document.getElementById('modal'))}
+      {createPortal(<Modal>{props.children}</Modal>, document.getElementById('modal'))}
+    </Fragment> 
+  )
+}
 
 export default Modal;
